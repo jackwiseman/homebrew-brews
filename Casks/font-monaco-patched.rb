63aa39231fa4a14f1be6d047446e3d6e1322bc95
Jack Wiseman <jackwiseman4@gmail.com>
2023-02-13 15:47:03 -0800

    feat: add patched monaco font

---
+++ Casks/font-monaco-patched.rb
@@ -0,0 +1,11 @@
+cask "font-monaco-patched" do
+  version ":latest"
+  sha256 "b7df23b2e9c9155cfaa983c171cf4c2af8627a7b4814e10899879fdf18bd918a"
+
+  url "https://github.com/Karmenzind/monaco-nerd-fonts/raw/master/fonts/Monaco%20Nerd%20Font%20Complete.ttf"
+  name "font-monaco-patched"
+  desc ""
+  homepage ""
+
+  font "Monaco Nerd Font Complete.ttf"
+end
