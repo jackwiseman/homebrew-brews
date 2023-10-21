9f3747fcf87e1bd8be159fd133e48b45bd2796ae
Jack Wiseman <40647382+jackwiseman@users.noreply.github.com>
2023-10-21 06:26:33 -0700

    versioning

--- Casks/font-monaco-patched.rb
+++ Casks/font-monaco-patched.rb
@@ -1,6 +1,6 @@
 cask "font-monaco-patched" do
-  version ":latest"
-  sha256 "4d98fc870f17d0b73d6691edf4fdbe108f5071219566637a7a522b1b0d27f82b"
+  version :latest
+  sha256 :no_check
 
   url "https://github.com/Karmenzind/monaco-nerd-fonts/raw/master/fonts/Monaco%20Nerd%20Font%20Complete.ttf"
   name "font-monaco-patched"
