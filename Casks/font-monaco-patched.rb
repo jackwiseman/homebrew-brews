74fba5eee17e3cb726007421cd7454c9ad383540
Jack Wiseman <40647382+jackwiseman@users.noreply.github.com>
2024-01-18 19:56:11 -0800

    pin monaco to old commit

--- Casks/font-monaco-patched.rb
+++ Casks/font-monaco-patched.rb
@@ -2,7 +2,7 @@ cask "font-monaco-patched" do
   version :latest
   sha256 :no_check
 
-  url "https://github.com/Karmenzind/monaco-nerd-fonts/raw/master/fonts/Monaco%20Nerd%20Font%20Complete.ttf"
+  url "https://github.com/Karmenzind/monaco-nerd-fonts/raw/afb41000f577b328081f93896fc7c0abd2857f14/fonts/Monaco%20Nerd%20Font%20Complete.ttf"
   name "font-monaco-patched"
   desc "Patched monaco nerd font"
   homepage "https://github.com/Karmenzind/monaco-nerd-fonts"
