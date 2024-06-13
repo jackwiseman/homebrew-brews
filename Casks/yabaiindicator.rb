ea36fed2647c64e3b85e78ff68f52ec0e6a5f9db
Jack Wiseman <40647382+jackwiseman@users.noreply.github.com>
2024-06-12 22:03:50 -0700

    add yabaiindicator

---
+++ Casks/yabaiindicator.rb
@@ -0,0 +1,11 @@
+cask "yabaiindicator" do
+  version "0.3.4"
+  sha256 "5dde182daebe3f442193232d3cdefbd66364235c7fa0490bbc5e67f42e1912ef"
+
+  url "https://github.com/xiamaz/YabaiIndicator/releases/download/0.3.4/YabaiIndicator-0.3.4.zip"
+  name "yabaiindicator"
+  desc "status bar indicator for yabai"
+  homepage "https://github.com/xiamaz/YabaiIndicator"
+
+  app "YabaiIndicator-0.3.4/YabaiIndicator.app"
+end
