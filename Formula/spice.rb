a23551a07690f193887499a64f950d30b39d4141
Jack Wiseman <40647382+jackwiseman@users.noreply.github.com>
2026-02-17 20:40:13 -0500

    update deps

--- Formula/spice.rb
+++ Formula/spice.rb
@@ -11,7 +11,13 @@ class Spice < Formula
   depends_on "intltool" => :build
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
+  depends_on "python@3" => :build
   depends_on "vala" => :build
+
+  resource "pyparsing" do
+    url "https://files.pythonhosted.org/packages/source/p/pyparsing/pyparsing-3.2.1.tar.gz"
+    sha256 "61980854fd66de3a90028d679a954d5f2623e83144b5afe5ee86f43d762e5f0a"
+  end
 
   depends_on "atk"
   depends_on "cairo"
@@ -36,6 +42,15 @@ class Spice < Formula
   depends_on "usbredir"
 
   def install
+    python3 = Formula["python@3"].opt_bin/"python3"
+
+    resource("pyparsing").stage do
+      system python3, "-m", "pip", "install",
+             "--no-build-isolation", "--no-deps",
+             "--prefix=#{buildpath}/.pyparsing", "."
+    end
+    ENV.prepend_path "PYTHONPATH", Dir["#{buildpath}/.pyparsing/lib/python*/site-packages"].first
+
     system "./autogen.sh"
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
